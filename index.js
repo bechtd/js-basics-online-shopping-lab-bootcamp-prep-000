c93653ee0c54aa0d40bdeb8b4b1f611f4a1a0156
David Becht <bechtd@gmail.com>
2018-04-17 20:02:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
   var newItem = { itemName: item, itemPrice: Math.floor(Math.random() * 100) }
-  cart.push(newItem);
+  getCart().push(newItem);
   return `${newItem.itemName} has been added to your cart.`;
 }
 
@@ -19,7 +19,16 @@ function viewCart() {
   if(cart.length === 0) {
     return "Your shopping cart is empty."
   }
+  var items = ""
+  for(var i = 0; i < cart.length; i++) {
+    if(items.length > 0) {
+      items += ","
+    }
+    
+    items += `${cart[i].itemName} at $${cart[i].itemPrice}`
+  }
   
+  return `In your cart, you have ${items}.`
 }
 
 function total() {
